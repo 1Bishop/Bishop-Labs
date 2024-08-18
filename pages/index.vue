15ae08bea33572ba7249a1acc1a5ad2961d6462b
1Bishop <chris@bishop.dev>
2024-08-18 11:42:48 -0400

    Getting routing working 😄

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const name = "Hello!"
+function sayHello(): void {
+  console.log(name)
+}
+</script>
+
+<template>
+  <h1>Hello Test</h1>
+  <p>{{name}}</p>
+  <button @click="sayHello">Say Hello</button>
+
+</template>
+
+<style scoped>
+
+</style>
